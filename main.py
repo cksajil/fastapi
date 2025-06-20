cb239e7faee63dd950f76a5f783ec0f8a88a6a4f
cksajil <sajilck@gmail.com>
2025-06-20 15:51:41 +0530

    Example 2: uppercase

--- main.py
+++ main.py
@@ -11,5 +11,10 @@ async def root():
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
-
     return {"item_id": item_id, "q": q}
+
+
+@app.get("/to_upper")
+def to_upper(text: str):
+    """Convert a query parameter 'text' to upper case."""
+    return {"original": text, "upper": text.upper()}
